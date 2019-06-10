6dcfdf7196d8354b5a0b3addf4492c43c0319f10
ashlionTimeless <41907807+ashlionTimeless@users.noreply.github.com>
2019-06-11 00:05:00 +0300

    Update enviroment.js

--- enviroment.js
+++ enviroment.js
@@ -6,11 +6,11 @@ if(!process.env.NODE_ENV){
 process.env.baseDir = '.';
 process.env.LAST_BLOCK_FILE='./blocknumber.txt';
 process.env.SERVER = "http://127.0.0.1";
-process.env.node_address_http='http://127.0.0.1:5545';
-process.env.node_address_ws='ws://127.0.0.1:6545';
-process.env.MONGODB_URL ='127.0.0.1:27017';
+process.env.node_address_http='http://95.217.45.242:5545';
+process.env.node_address_ws='ws://95.217.45.242:6545';
+process.env.MONGODB_URL ='127.0.0.1:47017';
 process.env.MONGODB_DB = 'mydb';
 process.env.logDir = '.';
 process.env.fileMode='0666';
 process.env.PORT=3000;
-process.env.ADMIN_ADDRESS="0x2c5B73805753165d791a3155b45B8B87F8cCF384";
+process.env.ADMIN_ADDRESS="0x82a7fc1c127a04bf1e261d71055c07ed5ad28855";
